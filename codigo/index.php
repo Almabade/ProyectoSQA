e7a53f9718cf9c0a2bf5fc9cc109ec496fbfd37d
JhuniorCq <holiver.ccora.quispe2@gmail.com>
2023-10-28 16:52:06 -0500

    Codigo Limpio (Aun no se borran los archivos de EDITAR-USUARIO)

--- codigo/index.php
+++ codigo/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>I.E. ALAN TURING</title>
 </head>
 <body>
     <h1>Autenticación ok</h1>
@@ -27,8 +27,6 @@
         else {
             header("location: login.php");
         }
-
-       
     ?>
 
     <a href="src/cerrar_sesion.php">cerrar sesion</a>
